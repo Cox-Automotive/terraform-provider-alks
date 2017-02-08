be1222f7544dc12ebd0749a0462d065f2efdc1ca
Brian <brian.antonelli@gmail.com>
2017-02-08 15:20:58 -0500

    Now creating IAM roles via Terraform! w00t! :boom:

--- resource_alks_iamrole.go
+++ resource_alks_iamrole.go
@@ -37,6 +37,22 @@ func resourceAlksIamRole() *schema.Resource {
 func resourceAlksIamRoleCreate(d *schema.ResourceData, meta interface{}) error {
     log.Printf("[DEBUG] ALKS IAM Role Create")
 
+    var roleName  = d.Get("name").(string)
+    var roleType  = d.Get("type").(string)
+    var incDefPol = d.Get("include_default_policies").(bool)
+
+    client := meta.(*AlksClient)
+    resp, err := client.CreateIamRole(roleName, roleType, incDefPol)
+
+    if err != nil {
+        return err
+    }
+
+    log.Printf("[INFO] Created role: %s with ARN: %s", resp.RoleName, resp.RoleArn)
+
+    d.SetId(resp.RoleArn)
+    d.Set("name", resp.RoleName)
+
     return nil
 }
 
