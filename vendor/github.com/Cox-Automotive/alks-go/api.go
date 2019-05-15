c8b648abc05d789290874605e0aee176fec57aac
Webb Barker <robert.barker@coxautoinc.com>
2019-05-15 15:07:10 -0400

    Updates alks-go to latest

--- vendor/github.com/Cox-Automotive/alks-go/api.go
+++ vendor/github.com/Cox-Automotive/alks-go/api.go
@@ -4,50 +4,57 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/go-cleanhttp"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
+
+	cleanhttp "github.com/hashicorp/go-cleanhttp"
 )
 
-// AlksAccount is used to represent the configuration for the ALKS client
-type AlksAccount struct {
-	Username string `json:"userid"`
-	Password string `json:"password"`
-	Account  string `json:"account"`
-	Role     string `json:"role"`
-}
-
-type AlksSTS struct {
-	AccessKey string `json:"accessKey"`
-	SecretKey string `json:"secretKey"`
-	Token     string `json:"sessionToken"`
+// AccountDetails represents the callers Account and Role information for ALKS requests
+type AccountDetails struct {
+	Account string `json:"account,omitempty"`
+	Role    string `json:"role,omitempty"`
 }
 
 // Client represents an ALKS client and contains the account info and base url.
 type Client struct {
-	Account AlksAccount
-	STS     AlksSTS
-	BaseURL string
+	Credentials    AuthInjecter
+	AccountDetails AccountDetails
+	BaseURL        string
 
-	Http *http.Client
+	http      *http.Client
+	userAgent string
+}
+
+// LoginRoleResponse represents the response from ALKS containing information about a login role
+type LoginRoleResponse struct {
+	BaseResponse
+	LoginRole LoginRole `json:"loginRole"`
+}
+
+// LoginRole represents information about a login role
+type LoginRole struct {
+	Account        string `json:"account"`
+	IamKeyActive   bool   `json:"iamKeyActive"`
+	MaxKeyDuration int    `json:"maxKeyDuration"`
+	Role           string `json:"role"`
 }
 
 // NewClient will create a new instance of the ALKS Client. If you don't yet know the account/role
 // pass them as nil and then invoke GetAccounts().
 func NewClient(url string, username string, password string, account string, role string) (*Client, error) {
+	creds := Basic{Username: username, Password: password}
+
 	client := Client{
-		Account: AlksAccount{
-			Username: username,
-			Password: password,
-			Account:  account,
-			Role:     role,
-		},
-		STS:     AlksSTS{},
-		BaseURL: url,
-		Http:    cleanhttp.DefaultClient(),
+		Credentials:    &creds,
+		AccountDetails: AccountDetails{Account: account, Role: role},
+		BaseURL:        url,
+		http:           cleanhttp.DefaultClient(),
+		userAgent:      "alks-go",
 	}
 
 	return &client, nil
@@ -55,21 +62,42 @@ func NewClient(url string, username string, password string, account string, rol
 
 // NewSTSClient will create a new instance of the ALKS Client using STS tokens.
 func NewSTSClient(url string, accessKey string, secretKey string, token string) (*Client, error) {
+	creds := STS{AccessKey: accessKey, SecretKey: secretKey, SessionToken: token}
 	client := Client{
-		Account: AlksAccount{},
-		STS: AlksSTS{
-			AccessKey: accessKey,
-			SecretKey: secretKey,
-			Token:     token,
-		},
-		BaseURL: url,
-		Http:    cleanhttp.DefaultClient(),
+		Credentials: &creds,
+		BaseURL:     url,
+		http:        cleanhttp.DefaultClient(),
+		userAgent:   "alks-go",
 	}
 
 	return &client, nil
 }
 
-// newRequest will create a new request object for API requests.
+// NewBearerTokenClient will create a new instance of the ALKS Client using Okta Bearer Token auth.
+func NewBearerTokenClient(url string, bearerToken string, account string, role string) (*Client, error) {
+	creds := Bearer{Token: bearerToken}
+
+	client := Client{
+		Credentials:    &creds,
+		AccountDetails: AccountDetails{Account: account, Role: role},
+		BaseURL:        url,
+		http:           cleanhttp.DefaultClient(),
+		userAgent:      "alks-go",
+	}
+
+	return &client, nil
+}
+
+// SetUserAgent sets the client user agent in order to report tool details to ALKS
+func (c *Client) SetUserAgent(userAgent string) {
+	if userAgent == "" {
+		return
+	}
+
+	c.userAgent = userAgent
+}
+
+// NewRequest will create a new request object for API requests.
 func (c *Client) NewRequest(json []byte, method string, endpoint string) (*http.Request, error) {
 	u, err := url.Parse(c.BaseURL + endpoint)
 
@@ -84,7 +112,12 @@ func (c *Client) NewRequest(json []byte, method string, endpoint string) (*http.
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", "alks-go")
+	req.Header.Set("User-Agent", c.userAgent)
+	err = c.Credentials.InjectAuth(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error adding configuring authentication: %s", err)
+	}
 
 	log.Println("------- ALKS HTTP Request -------")
 	requestDump, err := httputil.DumpRequest(req, true)
@@ -99,53 +132,70 @@ func (c *Client) NewRequest(json []byte, method string, endpoint string) (*http.
 
 // decodeBody will convert a http.Response object to a JSON object.
 func decodeBody(resp *http.Response, out interface{}) error {
-	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("------- ALKS HTTP Response -------")
+	responseDump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(responseDump))
+	log.Println("-------- !!!!!!!!!! ---------")
 
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
-	log.Println("------- ALKS HTTP Response -------")
-	log.Printf("Status code: %v", resp.StatusCode)
-	log.Println(string(body))
-	log.Println("-------- !!!!!!!!!! ---------")
-
 	if err = json.Unmarshal(body, &out); err != nil {
+		if resp.StatusCode >= 300 {
+			return fmt.Errorf("HTTP Status (%d): %s", resp.StatusCode, err)
+		}
 		return err
 	}
 
 	return nil
 }
 
-// checkResp will validate a http.Response based on its status code.
-func checkResp(resp *http.Response, err error) (*http.Response, error) {
-	if err != nil {
-		return resp, err
-	}
-
-	switch i := resp.StatusCode; {
-	case i == 200:
-		return resp, nil
-	case i == 201:
-		return resp, nil
-	case i == 202:
-		return resp, nil
-	case i == 204:
-		return resp, nil
-	case i == 400:
-		return nil, fmt.Errorf("API Error 400: %s", resp.Status)
-	case i == 401: // access denied will still return json
-		return resp, nil
-	case i == 402:
-		return nil, fmt.Errorf("API Error 402: %s", resp.Status)
-	case i == 422:
-		return nil, fmt.Errorf("API Error 422: %s", resp.Status)
-	default:
-		return nil, fmt.Errorf("API Error %d: %s", resp.StatusCode, resp.Status)
-	}
-}
-
 // Durations will provide the valid session durations
-func (c *Client) Durations() []int {
-	return []int{1, 2, 6, 12, 18}
+func (c *Client) Durations() ([]int, error) {
+	log.Printf("[INFO] Requesting allowed durations from ALKS")
+
+	// Use .../me endpoint for getting durations if using STS credentials
+	var path string
+	if len(strings.TrimSpace(c.AccountDetails.Account)) > 0 {
+		accountID := c.AccountDetails.Account[:12]
+		path = fmt.Sprintf("/loginRoles/id/%v/%v", accountID, c.AccountDetails.Role)
+	} else {
+		path = "/loginRoles/id/me"
+	}
+
+	req, err := c.NewRequest(nil, "GET", path)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	lrr := new(LoginRoleResponse)
+	err = decodeBody(resp, &lrr)
+	if err != nil {
+		if reqID := GetRequestID(resp); reqID != "" {
+			return nil, fmt.Errorf("Error parsing LoginRole response: [%s] %s", reqID, err)
+		}
+
+		return nil, fmt.Errorf("Error parsing LoginRole response: %s", err)
+	}
+
+	if lrr.RequestFailed() {
+		return nil, fmt.Errorf("Error fetching role information: [%s] %s", lrr.BaseResponse.RequestID, strings.Join(lrr.GetErrors(), ", "))
+	}
+
+	maxDuration := lrr.LoginRole.MaxKeyDuration
+	durations := make([]int, maxDuration)
+	for i := 0; i < maxDuration; i++ {
+		durations[i] = i + 1
+	}
+	return durations, nil
 }
