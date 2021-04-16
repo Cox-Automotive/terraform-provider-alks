0a9cde384a8988f7406886be95361a02b04011a9
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2021-04-16 15:49:40 -0400

    Update vendor.

---
+++ vendor/github.com/Cox-Automotive/alks-go/is_iam_enabled.go
@@ -0,0 +1,68 @@
+package alks
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+)
+
+type IsIamEnabledRequest struct {
+	AccountDetails
+	RoleArn string `json:"roleArn,omitempty"`
+}
+
+// IsIamEnabledResponse is used to represent a role that's IAM active or not.
+type IsIamEnabledResponse struct {
+	BaseResponse
+	AccountDetails
+	RoleArn    string `json:"roleArn"`
+	IamEnabled bool   `json:"iamEnabled"`
+}
+
+// IsIamEnabled will check if a MI, AccountDetails, or STS assumed role is IAM active or not.
+func (c *Client) IsIamEnabled(roleArn string) (*IsIamEnabledResponse, error) {
+
+	if len(roleArn) > 1 {
+		log.Printf("[INFO] Is IAM enabled for MI: %s", roleArn)
+	} else {
+		log.Printf("[INFO] Is IAM enabled for: %s/%s", c.AccountDetails.Account, c.AccountDetails.Role)
+	}
+
+	iam := IsIamEnabledRequest{
+		c.AccountDetails,
+		roleArn,
+	}
+
+	body, err := json.Marshal(iam)
+
+	if err != nil {
+		return nil, fmt.Errorf("error encoding IAM create role JSON: %s", err)
+	}
+
+	req, err := c.NewRequest(body, "POST", "/isIamEnabled")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	validate := new(IsIamEnabledResponse)
+	err = decodeBody(resp, validate)
+
+	if err != nil {
+		if reqID := GetRequestID(resp); reqID != "" {
+			return nil, fmt.Errorf("error parsing isIamEnabled response: [%s] %s", reqID, err)
+		}
+
+		return nil, fmt.Errorf("error parsing isIamEnabled response: %s", err)
+	}
+	if validate.RequestFailed() {
+		return nil, fmt.Errorf("error validating if IAM enabled: [%s] %s", validate.BaseResponse.RequestID, strings.Join(validate.GetErrors(), ", "))
+	}
+
+	return validate, nil
+}
