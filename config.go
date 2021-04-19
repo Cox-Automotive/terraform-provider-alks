5944731c1c997211c38272c3e2065095a2898090
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2021-04-19 14:14:19 -0400

    non-MI error handle.

--- config.go
+++ config.go
@@ -185,11 +185,7 @@ func (c *Config) Client() (*alks.Client, error) {
 
 	// Validate STS for IAM active.
 	validateSTS, err := client.IsIamEnabled("")
-	if err != nil {
-		return nil, err
-	}
-
-	if validateSTS.IamEnabled != true {
+	if err != nil || validateSTS.IamEnabled != true {
 		return nil, errors.New("Looks like you are not using ALKS IAM credentials. This will result in errors when creating roles. \n " +
 			"Note: If using ALKS CLI to get credentials, be sure to use the '-i' flag. \n Please see https://coxautoinc.sharepoint.com/sites/service-internal-tools-team/SitePages/ALKS-Terraform-Provider---Troubleshooting.aspx for more information.")
 	}
