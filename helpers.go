47965422ab4ec995bbf76ae96587a65b29e1fb88
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2021-09-14 10:47:05 -0400

    Improve error message when not using IAM active role.

--- helpers.go
+++ helpers.go
@@ -14,8 +14,8 @@ func validateIAMEnabled(client *alks.Client) error {
 	}
 
 	if !resp.LoginRole.IamKeyActive {
-		return errors.New("Looks like you are not using ALKS IAM credentials. This will result in errors when creating roles. \n " +
-			"Note: If using ALKS CLI to get credentials, be sure to use the '-i' flag. \n Please see https://coxautoinc.sharepoint.com/sites/service-internal-tools-team/SitePages/ALKS-Terraform-Provider---Troubleshooting.aspx for more information.")
+		return errors.New("uh oh! You're using the " + resp.LoginRole.Role + " which is not IAM active. " +
+			"Please instead use one of the following roles: Admin, IAMAdmin, LabAdmin, or a Machine Identity")
 	}
 
 	return nil
