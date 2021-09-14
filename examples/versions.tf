754535deb696fe8f35779f5486f15f2af669ff76
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2021-09-14 09:27:15 -0400

    Bump versions.

--- examples/versions.tf
+++ examples/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     alks = {
       source  = "Cox-Automotive/alks"
-      version = "2.0.4"
+      version = "2.0.5"
     }
     aws = {
       source = "hashicorp/aws"
