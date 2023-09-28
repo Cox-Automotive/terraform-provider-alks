9e33316dfa5b026a2bf8af72750bfad5e799f72b
Dave-Barcelo <dave.barcelo@coxautoinc.com>
2023-09-28 07:57:14 -0400

    Adding debug switch.

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
+	// For debugging provider, set the following environment variable
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
+		Debug:        debug,
+		ProviderAddr: "cox-automotive/alks",
 		ProviderFunc: func() *schema.Provider {
 			return Provider()
 		},
